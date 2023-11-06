c4a24eaf7b14be952f5d329fce5ebdc64436af55
mehmet ali <bm.m.alicetin@gmail.com>
2023-11-06 17:15:37 +0300

    unit test has been wrapped up by junit5 lambda.

--- src/main/java/com/cetin/java/junit/MathCalculate.java
+++ src/main/java/com/cetin/java/junit/MathCalculate.java
@@ -1,7 +1,16 @@
 package com.cetin.java.junit;
 
 public class MathCalculate {
+	private static final double PI = 3.14;
 	public int add(int num1, int num2){
 		return num1+num2;
+	}
+
+	public int divide(int num1, int num2){
+		return num1/num2;
+	}
+
+	public double competeCircleArea(int radius){
+		return PI*radius*radius;
 	}
 }
--- src/test/java/com/cetin/java/junit/MathCalculateTest.java
+++ src/test/java/com/cetin/java/junit/MathCalculateTest.java
@@ -1,21 +1,58 @@
 package com.cetin.java.junit;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 class MathCalculateTest {
 	MathCalculate mathCalculate;
+	Supplier<String> supplier = ()-> "This test has been fail!!";
 
+	TestInfo testInfo;
+
+	TestReporter testReporter;
+
+	@BeforeAll
+	public void beforeAll(){
+		System.out.println("Before all method calls");
+	}
 	@BeforeEach
-	void setUp() {
+	void setUp(TestInfo testInfo, TestReporter testReporter) {
+		this.testInfo = testInfo;
+		this.testReporter = testReporter;
 		mathCalculate = new MathCalculate();
 	}
 
 	@Test
+	@DisplayName(value = "Testing add method")
+	@Tag("Math")
 	void add() {
-		Assertions.assertEquals(5, mathCalculate.add(4,1));
+		System.out.println("Running " + testInfo.getDisplayName() + " with tags "+ testInfo.getTags());
+		Assertions.assertEquals(5, mathCalculate.add(4,1), supplier);
+	}
+
+	@Test
+	@RepeatedTest(3)
+	@Tag("Circle")
+	void competeCircleArea() {
+		testReporter.publishEntry("Running " + testInfo.getDisplayName() + " with tags "+ testInfo.getTags());
+		Assertions.assertEquals(314, mathCalculate.competeCircleArea(10), supplier);
+	}
+
+	@Test
+	@Tag("divide")
+	void divide() {
+		Assertions.assertThrows(ArithmeticException.class, ()-> mathCalculate.divide(1,0), supplier);
+	}
+
+
+	@Test
+	@Disabled
+	@DisplayName(value = "TDD test method")
+	void tddMethod(){
+		fail("This method should not run");
 	}
 }
